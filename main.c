fa6700494aa1bd619fceee2699f38de21495aaee
Preeti Bembey <74343887+akashbembey@users.noreply.github.com>
2021-12-15 19:31:13 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Enter your name : ");
+    char name[10];
+    scanf("%s",&name);
+    printf("Your name is %s", name);
+
+    return 0;
+}
